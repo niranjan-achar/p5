75e61cb45ce1e9234aa6261702d2687e695f1491
Niranjan Acharya <89630357+niranjan-achar@users.noreply.github.com>
2025-11-23 20:08:26 +0530

    Remove reportWebVitals and update render method

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import RegistrationForm from './RegisterationForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,4 +10,3 @@ root.render(
     <RegistrationForm />
   </React.StrictMode>
 );
-reportWebVitals();
